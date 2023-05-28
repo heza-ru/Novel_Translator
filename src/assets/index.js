8d5bbe0347348b1846792012808e00d4c5ddb51b
Mohammad Haider <stralende007@gmail.com>
2023-05-28 22:10:14 +0530

    application init

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import linkIcon from './link.svg'
+import enterIcon from './enter.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+
+export {
+    linkIcon,
+    enterIcon,
+    loader,
+    copy,
+    logo,
+    tick
+}
